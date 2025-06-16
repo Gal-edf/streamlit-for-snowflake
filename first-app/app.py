b6a6579aca974f35d59336ca11756bc1724c611d
GalShulerEDF <162106355+GalShulerEDF@users.noreply.github.com>
2025-06-16 10:02:01 +0300

    -update

---
+++ first-app/app.py
@@ -0,0 +1,23 @@
+import urllib.parse
+import pandas as pd
+import webbrowser
+import streamlit as st
+
+st.title('Hierarchical Data Viewer')
+
+df = pd.read_csv("data/employees.csv",header=0).convert_dtypes()
+st.dataframe(df)
+
+edges = ""
+for _, row in df.iterrows():
+    if not pd.isna(row.iloc[1]):
+        edges += f'\t"{row.iloc[0]}" -> "{row.iloc[1]}";\n'
+
+d = f'digraph {{\n{edges}}}'
+url = f'https://magjac.com/graphviz-visual-editor/?dot={urllib.parse.quote(d)}'
+st.graphviz_chart(d)
+
+
+#webbrowser.open(url)
+#st.graphviz_chart(d)
+
